bc42e60862e0a61ea6274adef918bd7797e9985f
Mahmoud Ahmed <m7moud1212001@gmail.com>
2024-03-12 23:00:03 +0200

    create firebase Settings done

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:chat_material3/firebase_options.dart';
 import 'package:chat_material3/provider/theme_provider.dart';
 import 'package:chat_material3/screens/auth/login_screen.dart';
 import 'package:chat_material3/utils/app_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MyApp(
       themeProvider: ThemeProvider(),
